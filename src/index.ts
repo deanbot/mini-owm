86e9fe59d5863eed86e882f1c9f337b893bb9601
Dean Verleger <deanverleger@gmail.com>
2020-05-27 16:56:35 -0500

    export interface and types in index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
+export { default as coords } from './coords';
+export { default as getAtts } from './getAtts';
+export { default as IMiniOwm } from './IMiniOwm';
 export { default as default } from './MiniOwm';
