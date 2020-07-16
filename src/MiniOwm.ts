06b5ef4c44205f1895cc2e187672573314e9eaae
Dean Verleger <deanverleger@gmail.com>
2020-07-16 14:21:58 -0500

    use lambdas. support 0 lat/long

--- src/MiniOwm.ts
+++ src/MiniOwm.ts
@@ -29,55 +29,55 @@ export class MiniOwm implements IMiniOwm {
     this._language = (language && language.trim()) ? language.trim().toLowerCase() : '';
   }
 
-  public apiKey(value: string) {
+  public apiKey = (value: string) => {
     this._apiKey = value;
     return this;
   }
 
-  public latitude(value: number) {
+  public latitude = (value: number) => {
     this._latitude = value;
     return this;
   }
 
-  public longitude(value: number) {
+  public longitude = (value: number) => {
     this._longitude = value;
     return this;
   }
 
-  public exclude(value: string) {
+  public exclude = (value: string) => {
     this._exclude = value;
     return this;
   }
 
   // temperature in Kelvin
   // wind in meter/sec
-  public standard() {
+  public standard = () => {
     this._units = Units.Standard;
     return this;
   }
 
   // temperature in Celsius
   // wind in meter/sec
-  public metric() {
+  public metric = () => {
     this._units = Units.Metric;
     return this;
   }
 
   // temperature in Fahrenheit
   // wind in miles/hour
-  public imperial() {
+  public imperial = () => {
     this._units = Units.Imperial;
     return this;
   }
 
   // set output of description field
   // see possible values here: https://openweathermap.org/api/one-call-api
-  public language(value:string) {
+  public language = (value:string) => {
     this._language = (value && value.trim()) ? value.trim().toLowerCase() : '';
     return this;
   }
 
-  public get(attributes?: getAtts) {
+  public get = (attributes?: getAtts) => {
     const {
       _latitude,
       _longitude,
@@ -88,12 +88,15 @@ export class MiniOwm implements IMiniOwm {
     } = this,
       atts = attributes || {},
       key = atts.apiKey || _apiKey,
-      lat = atts.coords ? atts.coords.latitude : _latitude,
-      lon = atts.coords ? atts.coords.longitude : _longitude,
+      lat = (atts.coords && atts.coords.latitude) ? atts.coords.latitude : _latitude,
+      lon = (atts.coords && atts.coords.longitude) ? atts.coords.longitude : _longitude,
       lang = (atts.language && atts.language.trim()) ? atts.language.trim().toLowerCase() : _language,
       units = (atts.units && atts.units.trim()) ? atts.units.trim().toLowerCase() : _units,
       part = atts.exclude || _exclude;
-    if (!key || !lat || !lon) {
+    if (!key 
+    	|| (!lat && lat !== 0) 
+    	|| (!lon && lon !== 0)
+   	) {
       throw new Error('Missing required parameters for Open Weather Map api call.');
     }
     let uri = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${key}`;
@@ -130,7 +133,7 @@ export class MiniOwm implements IMiniOwm {
   }
 
   // owm puts precip amount on a nested property
-  private getPrecipitation(value:any): number {
+  private getPrecipitation = (value:any): number => {
     return !value
       ? 0
       : (value['1h']
